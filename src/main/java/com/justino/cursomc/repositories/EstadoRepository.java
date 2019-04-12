31ff5cc8af1af0b063865790db1aa0a19bb0180f
Antonio Justino <antoniojustino@live.com>
2019-04-11 22:16:11 -0300

    Estado e Cidade

---
+++ src/main/java/com/justino/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.justino.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.justino.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
