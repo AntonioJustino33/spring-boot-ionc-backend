2ccf8eeb6dc9f646576527d14aaa4707626f74e2
Antonio Justino <antoniojustino@live.com>
2019-04-21 16:13:00 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/justino/cursomc/domain/Cliente.java
+++ src/main/java/com/justino/cursomc/domain/Cliente.java
@@ -13,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.justino.cursomc.domain.enums.TipoCliente;
@@ -36,6 +37,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 	}
@@ -104,6 +108,13 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -129,9 +140,6 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
 
+	
 }
