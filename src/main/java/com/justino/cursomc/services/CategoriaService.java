378692ac5c6a97b63d9a03d024e1ca32056f1904
Antonio Justino <antoniojustino@live.com>
2019-05-02 21:36:13 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/justino/cursomc/services/CategoriaService.java
+++ src/main/java/com/justino/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.justino.cursomc.domain.Categoria;
+import com.justino.cursomc.dto.CategoriaDTO;
 import com.justino.cursomc.repositories.CategoriaRepository;
 import com.justino.cursomc.services.exception.DataIntegrityException;
 import com.justino.cursomc.services.exception.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
